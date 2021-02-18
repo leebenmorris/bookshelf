b9b08bc2b83c87faea3f70ede61abadc038fb25f
Lee Morris <lee.morris@dazn.com>
2021-02-18 21:15:44 +0000

    2nd attempt

--- src/index.exercise.extra.1.lee.js
+++ src/index.exercise.extra.1.lee.js
@@ -3,60 +3,40 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { Dialog } from '@reach/dialog'
 import '@reach/dialog/styles.css'
-
 import { Logo } from './components/logo'
 
-const LoginModal = ({ showLoginModal, closeLoginModal }) => (
-    <Dialog isOpen={showLoginModal} onDismiss={closeLoginModal}>
-        <h2>Login</h2>
-        <button className="close-button" onClick={closeLoginModal}>
-            close
-        </button>
-    </Dialog>
-)
-
-const RegisterModal = ({ showRegisterModal, closeRegisterModal }) => (
-    <Dialog isOpen={showRegisterModal} onDismiss={closeRegisterModal}>
-        <h2>Register</h2>
-        <button
-            className="closeRegisterModal-button"
-            onClick={closeRegisterModal}
-        >
-            close
-        </button>
-    </Dialog>
-)
-
 const App = () => {
-    const [showLoginModal, setShowLoginModal] = React.useState(false)
-    const openLoginModal = () => setShowLoginModal(true)
-    const closeLoginModal = () => setShowLoginModal(false)
+    const [showModal, setShowModal] = React.useState('none')
 
-    const [showRegisterModal, setShowRegisterModal] = React.useState(false)
-    const openRegisterModal = () => setShowRegisterModal(true)
-    const closeRegisterModal = () => setShowRegisterModal(false)
-
-    const styles = {
-        button: {
-            display: 'block',
-        },
-    }
+    const closeModal = () => setShowModal('none')
 
     return (
         <>
-            <Logo />
+            <Logo width="100" height="100" />
             <h1>Bookshelf</h1>
-            <button onClick={openLoginModal} style={styles.button}>
-                Login
-            </button>
-            <button onClick={openRegisterModal} style={styles.button}>
-                Register
-            </button>
-            <LoginModal {...{ showLoginModal }} {...{ closeLoginModal }} />
-            <RegisterModal
-                {...{ showRegisterModal }}
-                {...{ closeRegisterModal }}
-            />
+            <button onClick={() => setShowModal('Login')}>Login</button>
+            <br />
+            <button onClick={() => setShowModal('Register')}>Register</button>
+            <Dialog
+                aria-label="Login form"
+                isOpen={showModal === 'Login'}
+                onDismiss={closeModal}
+            >
+                <h2>Login</h2>
+                <button className="close-button" onClick={closeModal}>
+                    close
+                </button>
+            </Dialog>
+            <Dialog
+                aria-label="Register form"
+                isOpen={showModal === 'Register'}
+                onDismiss={closeModal}
+            >
+                <h2>Register</h2>
+                <button className="close-button" onClick={closeModal}>
+                    close
+                </button>
+            </Dialog>
         </>
     )
 }

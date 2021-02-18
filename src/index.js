95b280878270782bfb56ec27b9584ec97615bcbc
Lee Morris <lee.morris@dazn.com>
2021-02-18 21:01:06 +0000

    import my version of the extra excersize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-export * from './index.final'
+// export * from './index.final'
 
 // export * from './index.exercise'
 
 // 💯 Use `@reach/dialog`
 // export * from './index.extra-1'
+
+// 💯 Use `@reach/dialog`
+export * from './index.exercise.extra.1.lee'
 
 // 💯 Create a LoginForm component
 // export * from './index.extra-2'
